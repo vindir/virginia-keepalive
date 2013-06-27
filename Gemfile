73c8a77a12a98dad06b32231cc860493c6ebbfe5
Jamey Owens <jowens@mojolingo.com>
2013-06-27 15:00:00 -0400

    Update repro to remove reel/app

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 gem 'adhearsion', :git => 'git://github.com/adhearsion/adhearsion.git', :branch => 'feature/punchblock_2.0'
 gem 'punchblock', :git => 'git://github.com/adhearsion/punchblock.git', :branch => 'develop'
-gem 'virginia'
+gem 'virginia', :git => 'git@github.com:polysics/virginia.git', :branch => 'old_handler'
 gem 'activesupport', '3.2.12'
 gem 'json'
 gem 'net-http-pipeline'
--- lib/stats/ahn_stats.rb
+++ lib/stats/ahn_stats.rb
@@ -1,19 +1,13 @@
 require 'reel'
-require 'reel/app'
 require 'json'
 
-class StatsHandler
-  include Reel::App
-
-  get '/status' do
+class AhnStatsHandler
+  def self.handle(request)
     response = {}
 
     response[:number_of_calls]   = Adhearsion.active_calls.size
     response[:status]            = Adhearsion::Process.state_name.to_s
-    [200, {}, response.to_json]
-  end
-
-  get '/' do
-    'Nothing defined for root'
+    [:ok, "#{response.to_json}"]
   end
 end
+
